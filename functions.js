54571a655bc042ae5d1aee3ea00f1a435d755e32
Dan Leedham <danleedham@googlemail.com>
2019-06-24 22:10:11 +0100

    Defence Sec Version

--- functions.js
+++ functions.js
@@ -1,135 +1,108 @@
-    
-    function getRandomMember() {
-         $.ajax({
-            url: 'https://cors-anywhere.herokuapp.com/https://api.parliament.uk/Live/fixed-query/person_photo_index?format=application%2Fjson',
-            dataType: 'JSON',
-            type: 'GET',
-            success: function (data) {
-                var m = Math.floor(Math.random() * (data.results.bindings.length));
-                
-                var imageId = data.results.bindings[m].image.value;
-                var imageId = imageId.replace('https://id.parliament.uk/','');
-                var imageURL = 'https://api.parliament.uk/Live/photo/'+imageId+'.jpeg?crop=CU_1:1&width=186&quality=80';
-                document.getElementById("image").src = imageURL;
-                var img = document.getElementById("image");
-                console.log('You\'ve requested the image for: '+data.results.bindings[m].displayAs.value);
-                if (img.complete) {
-                } else {
-                    img.addEventListener('load', imageLoaded(imageId))
-                    img.addEventListener('error', function() {})
-                }         
-                
-                correctAnswer = Math.floor(Math.random() * 4);
-                document.getElementById("name"+correctAnswer).innerHTML = data.results.bindings[m].displayAs.value;
-                document.getElementById("correctAnswer").value = correctAnswer;
-                for(i=0; i<4; i++){
-                    if(i !== correctAnswer){
-                        document.getElementById("name"+i).innerHTML = data.results.bindings[Math.floor(Math.random() * (data.results.bindings.length))].displayAs.value;
+function getDefenceMembers() {
+    var query = "https://id.parliament.uk/DSYZTjK2";
+    var endpoint = 'https://api.parliament.uk/query/government_position_index?format=application%2Fjson';
+    var queryUrl = endpoint + '&government_position_name=' + encodeURIComponent(query);
+    console.log(queryUrl);
+     $.ajax({
+        url: queryUrl,
+        dataType: 'JSON',
+        type: 'GET',
+        success: function (data) {
+            var allDefSecs = [], currentDefSec = [], currentDefMinisters = [];
+            for (i=0; i<data.results.bindings.length; i++){
+                if(data.results.bindings[i].government_position_name.value == "Secretary of State for Defence"){
+                    allDefSecs.push(data.results.bindings[i]);
+                    if(!data.results.bindings[i].hasOwnProperty("incumbency_end")){
+                        currentDefSec.push(data.results.bindings[i]);
+                    } 
+                } else if(data.results.bindings[i].government_position_name.value.includes("Ministry of Defence")) {
+                    if(!data.results.bindings[i].hasOwnProperty("incumbency_end")){
+                        currentDefMinisters.push(data.results.bindings[i]);
                     }
                 }
-                $('#names').removeClass('hidden');
-                
-                var mid = data.results.bindings[m].person.value;
-                var mid = mid.replace('https://id.parliament.uk/','');
-                correctConstituency = Math.floor(Math.random() * 4);
-                document.getElementById("correctConstituency").value = correctConstituency;
-                $.ajax({ 
-                    url: 'https://cors-anywhere.herokuapp.com/https://api.parliament.uk/Live/fixed-query/person_by_id?person_id='+mid+'&format=application%2Fjson', 
-                    dataType: 'JSON', 
-                    type: 'GET', 
-                    success: function (mdata) { 
-                        for(const prop in mdata){
-                            if(typeof mdata[prop]["https://id.parliament.uk/schema/constituencyGroupName"] != "undefined"){                   
-                                var constituency = mdata[prop]["https://id.parliament.uk/schema/constituencyGroupName"][0].value;
-                            }
-                        }
-                        document.getElementById("const"+correctConstituency).innerHTML = constituency;
-                    }
-                }); 
-                
-                
-                // Get some random constituency names
-                $.ajax({ 
-                    url: 'https://cors-anywhere.herokuapp.com/https://api.parliament.uk/Live/fixed-query/constituency_current?format=application%2Fjson', 
-                    dataType: 'JSON', 
-                    type: 'GET', 
-                    success: function (cdata) { 
-                        var constlist = Array();
-                        
-                        for(const prop in cdata){
-                            if(typeof cdata[prop]["https://id.parliament.uk/schema/constituencyGroupName"] != "undefined"){  
-                                var currentconst = cdata[prop]["https://id.parliament.uk/schema/constituencyGroupName"][0].value;                 
-                                constlist.push(currentconst);
-                            }
-                        }
-                        for(i=0; i<4; i++){
-                            if(i !== correctConstituency){
-                                document.getElementById("const"+i).innerHTML = constlist[Math.floor(Math.random() * constlist.length)];
-                            }
-                        }       
-                    }
-                }); 
-               
-                                       
-                               
             }
-        });
-    }
-    
-    function imageLoaded(imageId){
-        var img2 = document.getElementById("secondImage");
-        var imageURL = 'https://api.parliament.uk/Live/photo/'+imageId+'.jpeg?crop=CU_1:1&width=500&quality=80';
-        document.getElementById("secondImage").src = imageURL;
-        if (img2.complete) {
-        } else {
-            img2.addEventListener('load', function() {
-                document.getElementById("image").src = imageURL;
-                console.log('Loaded high quality image');
-            })
-            img2.addEventListener('error', function() {})
-        }               
-    }
-    
-    function checkname(answer) {
-        var correctAnswer =  document.getElementById("correctAnswer").value;
-        if(answer == correctAnswer){
-            var correctElement = document.getElementById("name"+correctAnswer);
-            correctElement.classList.add("btn-success");
-            correctElement.classList.remove("btn-secondary");
-            setTimeout(function(){
-                // getRandomMember();
-                correctElement.classList.add("btn-secondary");
-                correctElement.classList.remove("btn-success");
-                $('.btn-danger').addClass('btn-secondary');
-                $('.btn-danger').removeClass('btn-danger');
-                $('#names').addClass('hidden');
-                $('#constituencies').removeClass('hidden');
-            }, 2000);
-        } else {
-            var incorrectElement = document.getElementById("name"+answer);
-            incorrectElement.classList.add("btn-danger");
-            incorrectElement.classList.remove("btn-secondary");
+
+            document.getElementById("Label").innerHTML = currentDefSec[0].person_label.value;
+            var mnis = currentDefSec[0].person_mnis_member_id;
+            var startDate = currentDefSec[0].incumbency_start.value;
+            // document.getElementById("StartDate").innerHTML = "From: " + startDate.replace("+01:00","");
+            var person_uri = currentDefSec[0].person_uri.value;
+
+            console.log(currentDefMinisters);
+            
+            for(i=0; i<currentDefMinisters.length; i++){
+                var div = document.createElement("div");
+                div.setAttribute("id","minister"+i);
+                div.setAttribute("class","ministerElement")
+                var name = currentDefMinisters[i].person_label.value
+                var position = currentDefMinisters[i].government_position_name.value
+                var startDate = currentDefSec[0].incumbency_start.value;
+                startDate.replace("+01:00","");
+                div.innerHTML = '<p><span class="ministerName"> '+ name + ' </span><br /><span class="ministerPosition">' + position + '</span></p>';
+                document.getElementById("ministerList").appendChild(div);
+            }
+
+            $.ajax({ 
+                url: 'https://cors-anywhere.herokuapp.com/https://api.parliament.uk/query/person_photo_index.json', 
+                dataType: 'JSON', 
+                type: 'GET', 
+                success: function (mdata) { 
+                    console.log(mdata);
+                    var imageFound = false;
+                    for(i=0; i<mdata.results.bindings.length; i++){ 
+                        if(mdata.results.bindings[i].person.value == person_uri) {
+                            var imageId = mdata.results.bindings[i].image.value;
+                            var imageId = imageId.replace('https://id.parliament.uk/','');
+                            var imageURL = 'https://api.parliament.uk/photo/'+imageId+'.jpeg?crop=CU_1:1&width=186&quality=80';
+                            document.getElementById("image").src = imageURL;
+                            var img = document.getElementById("image");
+                            console.log('You\'ve requested the image for: '+mdata.results.bindings[i].displayAs.value);
+                            if (img.complete) {
+                            } else {
+                                img.addEventListener('load', imageLoaded(imageId))
+                                img.addEventListener('error', function() {})
+                            }
+                            var imageFound = true;        
+                        }
+                    }
+                   if(imageFound != true){
+                       console.log("No Official Portrait, loading a backup");
+                       person_uri = "https://id.parliament.uk/lcCpkkJg";
+                       for(i=0; i<mdata.results.bindings.length; i++){ 
+                        if(mdata.results.bindings[i].person.value == person_uri) {
+                            var imageId = mdata.results.bindings[i].image.value;
+                            var imageId = imageId.replace('https://id.parliament.uk/','');
+                            var imageURL = 'https://api.parliament.uk/photo/'+imageId+'.jpeg?crop=CU_1:1&width=186&quality=80';
+                            document.getElementById("image").src = imageURL;
+                            var img = document.getElementById("image");
+                            console.log('You\'ve requested the image for: '+mdata.results.bindings[i].displayAs.value);
+                            if (img.complete) {
+                            } else {
+                                img.addEventListener('load', imageLoaded(imageId))
+                                img.addEventListener('error', function() {})
+                            }
+                            var imageFound = true;        
+                        }
+                    }
+                   }
+                            
+                }
+            }); 
+             
         }
-    }
-    
-    function checkconst(answer) {
-        var correctConstituency =  document.getElementById("correctConstituency").value;
-        if(answer == correctConstituency){
-            var correctElement = document.getElementById("const"+correctConstituency);
-            correctElement.classList.add("btn-success");
-            correctElement.classList.remove("btn-secondary");
-            setTimeout(function(){
-                getRandomMember();
-                correctElement.classList.add("btn-secondary");
-                correctElement.classList.remove("btn-success");
-                $('.btn-danger').addClass('btn-secondary');
-                $('.btn-danger').removeClass('btn-danger');
-                $('#constituencies').addClass('hidden');
-                document.getElementById("image").src = 'images/blankface.jpg';
-            }, 2000);
-        } else {
-            var incorrectElement = document.getElementById("const"+answer);
-            incorrectElement.classList.add("btn-danger");
-            incorrectElement.classList.remove("btn-secondary");
-        }
-    }
+    });
+}
+
+function imageLoaded(imageId){
+    var img2 = document.getElementById("secondImage");
+    var imageURL = 'https://api.parliament.uk/photo/'+imageId+'.jpeg?crop=CU_1:1&width=500&quality=80';
+    document.getElementById("secondImage").src = imageURL;
+    if (img2.complete) {
+    } else {
+        img2.addEventListener('load', function() {
+            document.getElementById("image").src = imageURL;
+            console.log('Loaded high quality image');
+        })
+        img2.addEventListener('error', function() {})
+    }               
+}
